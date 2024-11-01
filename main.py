3f284432637d2d436ead008871e05d76f9d0e91b
charan <charanvcvs@gmail.com>
2024-11-01 10:26:41 +0530

    added pause functionality

--- main.py
+++ main.py
@@ -14,27 +14,40 @@ snake = Snake()
 food = Food()
 score_board = Scoreboard()
 
+
+def toggle_pause():
+    global is_pause
+    is_pause = not is_pause
+
+
 screen.listen()
 screen.onkey(snake.up, "Up")
 screen.onkey(snake.down, "Down")
 screen.onkey(snake.left, "Left")
 screen.onkey(snake.right, "Right")
-screen.onkey(snake.pause, "space")
+screen.onkey(toggle_pause, "space")
 
 game_is_on = True
-while game_is_on:
-    screen.update()
-    time.sleep(0.1)
-    snake.move()
+is_pause = False
+def game_loop():
+    if not is_pause:
+        screen.update()
+        snake.move()
 
-#detecting the food
-    if snake.head.distance(food) < 20:
-        food.refresh()
-        snake.extend()
-        score_board.add_score()
+        # Detecting the food
+        if snake.head.distance(food) < 20:
+            food.refresh()
+            snake.extend()
+            score_board.add_score()
 
-#detect collisiion with wall
-    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
-        game_is_on = False
-        score_board.game_over()
+        # Detect collision with wall
+        if (snake.head.xcor() > 280 or snake.head.xcor() < -280 or
+            snake.head.ycor() > 280 or snake.head.ycor() < -280):
+            score_board.game_over()
+            return  # End game loop if collision occurs
+
+    # Continue game loop after 100 ms
+    screen.ontimer(game_loop, 100)
+game_loop()
+
 screen.exitonclick()
